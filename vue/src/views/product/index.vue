92c213e6d29d79da3a860a1ec4f7c732846904ca
chrisyang2003 <1017975501@qq.com>
2022-04-27 02:31:57 +0800

    add page3

--- vue/src/views/product/index.vue
+++ vue/src/views/product/index.vue
@@ -1,45 +1,90 @@
 <template>
   <div class="app-container">
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      element-loading-text="Loading"
-      border
-      fit
-      highlight-current-row
-    >
-      <el-table-column align="center" label="ID" width="95">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          />
+          <div style="padding: 14px">
+            <span>好吃的汉堡</span>/<span>新</span>
+          </div>
+        </el-card>
+      </el-col>
+      <el-card>
+        <el-col :span="12">
+          你的出价
+          <el-form
+            ref="form"
+            :model="form"
+            label-width="80px"
+            label-position="top"
+          >
+            <el-form-item label="输入您的出价">
+              <el-input
+                v-model="form.price"
+                placeholder="拍卖出价 > 起拍价格"
+              ></el-input>
+            </el-form-item>
+
+            <el-form-item label="输入您发送的ETH">
+              <el-input
+                v-model="form.eth"
+                placeholder="发送的ETH >= 拍卖出价"
+              ></el-input>
+            </el-form-item>
+
+            <el-form-item label="设置加密密钥">
+              <el-input v-model="form.pw" placeholder="输入加密密钥"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+              <el-button type="primary" click="">确认拍卖</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+        <el-col :span="12">
+          Reveal Bid
+          <el-form
+            ref="form"
+            :model="form2"
+            label-width="80px"
+            label-position="top"
+          >
+            <el-form-item label="Amount You Bid">
+              <el-input
+                v-model="form2.bid"
+                placeholder="Amount > Start Price"
+              ></el-input>
+            </el-form-item>
+
+            <el-form-item label="Enter Secret Text">
+              <el-input
+                v-model="form2.secret"
+                placeholder="Any random text"
+              ></el-input>
+            </el-form-item>
+
+            <el-form-item label="user-number">
+              <el-input v-model="form2.number" placeholder="1..2..3"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+              <el-button type="primary" click="">Reveal Bid</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-card>
+    </el-row>
+
+    <el-card class="desc" style="margin-top: 10px">
+      <div slot="header" class="clearfix">
+        <span>物品描述</span>
+      </div>
+      <div>内容</div>
+    </el-card>
+    
   </div>
 </template>
 
@@ -47,33 +92,29 @@
 // import { getList } from '@/api/table'
 
 export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
   data() {
     return {
+      form: {
+        price: null,
+        pw: null,
+        eth: null,
+      },
+
+      form2: {
+        bid: null,
+        secret: null,
+        number: null,
+      },
+
       list: null,
-      listLoading: true
-    }
-  },
-  created() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData() {
-      this.listLoading = true
-      getList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
-    }
+      listLoading: true,
+    };
   }
-}
+};
 </script>
+
+<style>
+.image {
+  width: 50%;
+}
+</style>
